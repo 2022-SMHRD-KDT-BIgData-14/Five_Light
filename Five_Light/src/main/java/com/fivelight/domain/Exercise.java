01487fece46bd3e2f362aafcb337a97e9952117f
smhrd <smhrd@119.200.31.237>
2022-10-08 09:22:44 +0900

    221008_2nd commit

---
+++ Five_Light/src/main/java/com/fivelight/domain/Exercise.java
@@ -0,0 +1,12 @@
+package com.fivelight.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Exercise {
+	private String ex_name;
+}
